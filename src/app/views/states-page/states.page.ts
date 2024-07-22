01130ab0bd0d9faa0076d66a6f312b3d0cc00576
UldrenWEB <uldrenmiguel33@gmail.com>
2024-07-22 00:51:50 -0400

    fix(views && components): Se arreglaron bugs del component de estado y su vista

--- src/app/views/states-page/states.page.ts
+++ src/app/views/states-page/states.page.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICarouselItem } from 'src/app/components/carousel-component/icarousel-item.metadata';
+import { CAROUSEL_DATA_ITEMS } from 'src/app/components/state-component/carousel.const';
 
 @Component({
   selector: 'app-states',
   templateUrl: 'states.page.html',
   styleUrls: ['states.page.scss']
 })
-export class StatesPage {
+export class StatesPage implements OnInit {
+  items: ICarouselItem[] = CAROUSEL_DATA_ITEMS;
+  states: Array<{_id: string, user: {_id: string, username: string}, multimedia: Array<{url: string, type: 'image'}>, description: string}> = [];
 
   constructor() {}
+
+  //Aqui hara la consulta a los estados cada que cargue el componente
+  ngOnInit(): void {
+    this.states = [
+      {_id: 'fdsalkfjal', user: {
+        _id: 'fadfajlksdal',
+        username: 'Uldren Gedde',
+      }, 
+      multimedia: [
+        {
+        url: 'https://unavatar.io/geddeu',
+        type: 'image'
+      },
+        {
+        url: 'https://unavatar.io/geddeu',
+        type: 'image'
+      },
+        {
+        url: 'https://unavatar.io/geddeu',
+        type: 'image'
+      }
+    ],
+      description: 'Mi mensaje iria aqui para explicar el estado'
+    },
+      {_id: 'fdsalkfjal', user: {
+        _id: 'fadfajlksdal',
+        username: 'Uldren Gedde',
+      }, 
+      multimedia: [{
+        url: 'https://unavatar.io/erikatourt',
+        type: 'image'
+      }],
+      description: 'Mi mensaje iria aqui para explicar el estado'
+    },
+      {_id: 'fdsalkfjal', user: {
+        _id: 'fadfajlksdal',
+        username: 'Uldren Gedde',
+      }, 
+      multimedia: [{
+        url: 'https://unavatar.io/pedro',
+        type: 'image'
+      }],
+      description: 'Mi mensaje iria aqui para explicar el estado'
+    },
+    
+    ]
+  }
+
+  openModal(id: string) {
+    const modalTrigger = document.getElementById(`open-modal-${this.items}`) as HTMLIonModalElement;
+    modalTrigger?.click();
+  }
 
 }
